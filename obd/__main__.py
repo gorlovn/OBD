52323e3ad9be1966b8b5f67f19dc274e2a6e4c0d
Nikolay Gorlov <gnv@ctmed.ru>
2014-03-05 18:08:10 +0700

    OBDDetails Form created

--- obd/__main__.py
+++ obd/__main__.py
@@ -2,8 +2,16 @@
 # encoding: utf8
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class OBDDetailsForm(BoxLayout):
+
+    pass
+
 
 class obd(App):
+
     pass
 
 obd().run()
