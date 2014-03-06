b5e0d73604d3e3f6f989f4ed95956f35dec103a4
Nikolay Gorlov <gnv@ctmed.ru>
2014-03-06 17:02:04 +0700

    files moved to the odb directory

--- obd/obd-2.py
+++ obd/obd-2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# obd-2.py - обращение к OBD сервису с использованием 
+# obd-2.py - обращение к OBD сервису с использованием
 #            OBDConnection class
 #
 from obd2 import OBDConnection
 
 if __name__ == "__main__":
-    
+
     obdLink = OBDConnection()
-    
