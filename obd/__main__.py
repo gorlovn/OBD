2316ca8a8bd25d82ccc5fa7297d822644db74459
Nikolay Gorlov <gnv@ctmed.ru>
2014-03-06 12:46:06 +0700

    basic form - code interaction created

--- obd/__main__.py
+++ obd/__main__.py
@@ -3,15 +3,27 @@
 
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
 
 
 class OBDDetailsForm(AnchorLayout):
+    speed_box = ObjectProperty()
+    rpm_box = ObjectProperty()
+    throttle_box = ObjectProperty()
+    temp_box = ObjectProperty()
+    time_box = ObjectProperty()
 
-    pass
+    def connect(self):
+        print(self.speed_box.text)
+        print(self.rpm_box.text)
+        print(self.throttle_box.text)
+        print(self.temp_box.text)
+        print(self.time_box.text)
 
 
-class obd(App):
+class OBD(App):
 
-    pass
+    def connect_to_obd(self):
+        pass
 
-obd().run()
+OBD().run()
