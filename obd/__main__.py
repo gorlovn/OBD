32651297fda08b51819ecf19c144fd6c3eaab1b2
Nikolay Gorlov <gnv@ctmed.ru>
2014-03-12 19:35:00 +0700

    <Try Again> button added to the ConnectionModal class

--- obd/__main__.py
+++ obd/__main__.py
@@ -6,6 +6,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.modalview import ModalView
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 from obdII import OBDConnection
 from obdII import OBDException
 
@@ -16,6 +17,20 @@ class ConnectionModal(ModalView):
             anchor_y="bottom")
         self.label = Label(text="Connecting to OBD...")
         self.add_widget(self.label)
+        self.on_open = self.connect_to_obd
+
+    def connect_to_obd(self):
+        app = OBD.get_running_app()
+        try:
+            app.connect_to_obd()
+            self.label.text = "Connected!"
+        except OBDException:
+            self.label.text = "Sorry, couldn't connect"
+            button = Button(text="Try Again")
+            button.size_hint = (1.0, None)
+            button.height = "40dp"
+            button.bind(on_press=self.dismiss)
+            self.add_widget(button)
 
 
 class OBDDetailsForm(AnchorLayout):
@@ -50,6 +65,10 @@ class OBDDetailsForm(AnchorLayout):
 
 
 class OBD(App):
+    def __init__(self):
+        super(OBD, self).__init__()
+        self.obdLink = None
+        self.state = None
 
     def connect_to_obd(self):
         self.obdLink = OBDConnection()
